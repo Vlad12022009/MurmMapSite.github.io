7d6b287ac579d321859ca442500f645830d67159
Vlad12022009 <84780546+Vlad12022009@users.noreply.github.com>
2025-09-16 19:08:04 +0300

    New styles for map

--- scrypt.js
+++ scrypt.js
@@ -174,7 +174,8 @@ function init() {
     map = new ymaps.Map('map', {
         center: [68.964319, 33.048633],
         zoom: 10,
-        type: 'yandex#map'
+        type: 'yandex#map',
+        mode: 'vector'
     }, {
         restrictMapArea: [
             [66.141938, 26.980456],
@@ -183,6 +184,183 @@ function init() {
         minZoom: 3,
         yandexMapDisablePoiInteractivity: true
     });
+
+    const layer = new YMapDefaultSchemeLayer({
+        customization: [
+            {
+                "tags": {
+                    "any": [
+                        "transit"
+                    ]
+                },
+                "elements": [
+                    "label.icon",
+                    "label.text"
+                ],
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "outdoor",
+                        "park",
+                        "cemetery",
+                        "medical"
+                    ]
+                },
+                "elements": "label",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "poi",
+                    "none": [
+                        "outdoor",
+                        "park",
+                        "cemetery",
+                        "medical"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "road"
+                },
+                "types": "point",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "food_and_drink",
+                        "shopping",
+                        "commercial_services"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "traffic_light"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "entrance"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road"
+                    ],
+                    "none": [
+                        "road_1",
+                        "road_2",
+                        "road_3",
+                        "road_4",
+                        "road_5",
+                        "road_6",
+                        "road_7"
+                    ]
+                },
+                "elements": "label.icon",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_3",
+                        "road_4"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_5",
+                        "road_6"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_7"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_construction",
+                        "road_minor",
+                        "road_unclassified",
+                        "path",
+                        "road_limited"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "transit"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            }
+        ]
+    });
+    map.addChild(layer);
+
+
 
     map.panes.get('copyrights').getElement().style.display = 'none';
 

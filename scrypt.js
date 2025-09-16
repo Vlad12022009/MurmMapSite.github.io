f9b43c284021b38dc8ba33ad59e6239408c469dc
Vlad12022009 <84780546+Vlad12022009@users.noreply.github.com>
2025-09-16 19:32:53 +0300

    new styles

--- scrypt.js
+++ scrypt.js
@@ -19,10 +19,185 @@ async function initMap() {
 
                 // Уровень масштабирования
                 zoom: 10
-            }
+            },
+            mode: 'vector'
         }
     );
+    const layer = new YMapDefaultSchemeLayer({
+        customization: [
+            {
+                "tags": {
+                    "any": [
+                        "transit"
+                    ]
+                },
+                "elements": [
+                    "label.icon",
+                    "label.text"
+                ],
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "outdoor",
+                        "park",
+                        "cemetery",
+                        "medical"
+                    ]
+                },
+                "elements": "label",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "poi",
+                    "none": [
+                        "outdoor",
+                        "park",
+                        "cemetery",
+                        "medical"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": "road"
+                },
+                "types": "point",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "food_and_drink",
+                        "shopping",
+                        "commercial_services"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "traffic_light"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "entrance"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road"
+                    ],
+                    "none": [
+                        "road_1",
+                        "road_2",
+                        "road_3",
+                        "road_4",
+                        "road_5",
+                        "road_6",
+                        "road_7"
+                    ]
+                },
+                "elements": "label.icon",
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_3",
+                        "road_4"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_5",
+                        "road_6"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_7"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "road_construction",
+                        "road_minor",
+                        "road_unclassified",
+                        "path",
+                        "road_limited"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            },
+            {
+                "tags": {
+                    "any": [
+                        "transit"
+                    ]
+                },
+                "stylers": {
+                    "visibility": "off"
+                }
+            }
+        ]
+    });
+    map.addChild(layer);
 
     // Добавляем слой для отображения схематической карты
-    map.addChild(new YMapDefaultSchemeLayer());
+
 }

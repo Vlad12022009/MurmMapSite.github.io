eea24ca815452608635abcc46625786997dcf2c7
Vlad12022009 <84780546+Vlad12022009@users.noreply.github.com>
2025-09-16 19:29:34 +0300

    12556

--- scrypt.js
+++ scrypt.js
@@ -1,78 +1,28 @@
-ymaps3.ready.then(() => {
-    const { YMapDefaultSchemeLayer } = ymaps3.layer;
+initMap();
 
-    const map = new ymaps3.Map(document.getElementById('map'), {
-        location: {
-            center: [37.617635, 55.755814], // Москва
-            zoom: 10,
-            mode: 'vector'
-        }
-    });
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
 
-    const layer = new YMapDefaultSchemeLayer({
-        customization: [
-            {
-                tags: { any: ["transit"] },
-                elements: ["label.icon", "label.text"],
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["outdoor", "park", "cemetery", "medical"] },
-                elements: "label",
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: "poi", none: ["outdoor", "park", "cemetery", "medical"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: "road" },
-                types: "point",
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["food_and_drink", "shopping", "commercial_services"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["traffic_light"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["entrance"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["road"], none: ["road_1", "road_2", "road_3", "road_4", "road_5", "road_6", "road_7"] },
-                elements: "label.icon",
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["road"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["road_3", "road_4"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["road_5", "road_6"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["road_7"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["road_construction", "road_minor", "road_unclassified", "path", "road_limited"] },
-                stylers: { visibility: "off" }
-            },
-            {
-                tags: { any: ["transit"] },
-                stylers: { visibility: "off" }
+    const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
+
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [37.588144, 55.733842],
+
+                // Уровень масштабирования
+                zoom: 10
             }
-        ]
-    });
+        }
+    );
 
-    map.addChild(layer);
-});
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+}
